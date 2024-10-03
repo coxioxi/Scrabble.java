2a2314aea2823cad382dbce64e91423de71ab037
JyelMason <mason3@otterbein.edu>
2024-10-03 18:03:39 -0400

    Adds more methods for board class

--- code/Board.java
+++ code/Board.java
@@ -1,7 +1,7 @@
 
 import java.awt.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.List;
 
 public class Board {
     private final Tile[][] board;
@@ -18,6 +18,7 @@ public class Board {
         int point6 = 8;
         int point7 = 10;
         int point0 = 0;
+        letterKeyValue.put(" ",point0);
         letterKeyValue.put("A",point1);
         letterKeyValue.put("E",point1);
         letterKeyValue.put("I",point1);
@@ -44,19 +45,57 @@ public class Board {
         letterKeyValue.put("X",point6);
         letterKeyValue.put("Q",point7);
         letterKeyValue.put("Z",point7);
-        letterKeyValue.put(" ",point0);
+
 
     }
     public void addToBoard(String letter, int row, int column) {
-        board[row][column] = new Tile(letter,letterKeyValue.get(letter), new Point(row,column));
-
+        board[row][column] = new Tile(letter.toUpperCase(),letterKeyValue.get(letter.toUpperCase()), new Point(row,column));
     }
 
     public static void main(String[] args) {
         Board test = new Board();
+        test.addToBoard("r",2,9);
+        test.addToBoard("u",3,9);
+        test.addToBoard("n",4,9);
+        test.addToBoard("i",4,10);
+        test.addToBoard("g",4,11);
+        test.addToBoard("h",4,12);
+        test.addToBoard("t",4,13);
+        test.boardView();
+        System.out.println();
+    }
+    public void boardView(){
+        for(int row = 0; row < board.length; ++row){
+            for(int column = 0; column < board[row].length; ++column){
+                if(board[row][column] != null){
+                    System.out.print(" "+board[row][column].getLetter()+" ");
+                }
+                else{
+                    System.out.print(" X ");
+                }
+            }
+            System.out.println();
+        }
     }
     public void boardScan(){
-        
+        List<Tile> Tiles = new ArrayList<>();
+        for(int row = 0; row < board.length; ++row){
+            for(int column = 0; column < board[row].length; ++column){
+                if(board[row][column] != null){
+                    Tiles.add(board[row][column]);
+                }
+            }
+        }
+        //
+        for( int i = 0; i < Tiles.size(); ++i){
+            for(int j = 0; j < Tiles.size(); ++j){
+                
+            }
+        }
     }
-
-}
+    public void clearBoard(){
+        for(int row = 0; row < board.length; ++row){
+            Arrays.fill(board[row], null);
+            }
+        }
+    }
