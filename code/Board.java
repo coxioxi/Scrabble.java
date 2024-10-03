a0d2172c6dac7ea6fbab620ae1481b5989e0a388
JyelMason <mason3@otterbein.edu>
2024-10-03 16:19:42 -0400

    adds the hashmap for letter score values and creation of boardscan method for checking tile values.

--- code/Board.java
+++ code/Board.java
@@ -1,13 +1,62 @@
-import java.lang.reflect.Array;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Board {
-    public static void main(String[] args) {
-        Tile[][] board = new Tile[15][15];
+    private final Tile[][] board;
+    private final Map<String,Integer> letterKeyValue = new HashMap<>();
+    private final Map<Point,String> boardSpecialCell =  new HashMap<>();
+
+    public Board(){
+        board = new Tile[15][15];
+        int point1 = 1;
+        int point2 = 2;
+        int point3 = 3;
+        int point4 = 4;
+        int point5 = 5;
+        int point6 = 8;
+        int point7 = 10;
+        int point0 = 0;
+        letterKeyValue.put("A",point1);
+        letterKeyValue.put("E",point1);
+        letterKeyValue.put("I",point1);
+        letterKeyValue.put("L",point1);
+        letterKeyValue.put("N",point1);
+        letterKeyValue.put("O",point1);
+        letterKeyValue.put("U",point1);
+        letterKeyValue.put("S",point1);
+        letterKeyValue.put("T",point1);
+        letterKeyValue.put("R",point1);
+        letterKeyValue.put("D",point2);
+        letterKeyValue.put("G",point2);
+        letterKeyValue.put("B",point3);
+        letterKeyValue.put("C",point3);
+        letterKeyValue.put("M",point3);
+        letterKeyValue.put("P",point3);
+        letterKeyValue.put("F",point4);
+        letterKeyValue.put("H",point4);
+        letterKeyValue.put("V",point4);
+        letterKeyValue.put("W",point4);
+        letterKeyValue.put("Y",point4);
+        letterKeyValue.put("K",point5);
+        letterKeyValue.put("J",point6);
+        letterKeyValue.put("X",point6);
+        letterKeyValue.put("Q",point7);
+        letterKeyValue.put("Z",point7);
+        letterKeyValue.put(" ",point0);
 
     }
-    public Array addToBoard(Array[][] board, Tile letter, int row, int column) {
+    public void addToBoard(String letter, int row, int column) {
+        board[row][column] = new Tile(letter,letterKeyValue.get(letter), new Point(row,column));
 
-        return null;
+    }
+
+    public static void main(String[] args) {
+        Board test = new Board();
+    }
+    public void boardScan(){
+        
     }
 
 }
--- code/Tile.java
+++ code/Tile.java
@@ -2,16 +2,16 @@ import java.awt.*;
 
 public class Tile {
     private final int score;
-    private final char letter;
+    private final String letter;
     private final Point location;
     private boolean isNew;
-    public Tile(char letter, int score,Point location){
+    public Tile(String letter, int score,Point location){
         this.letter = letter;
         this.score = score;
         this.location = location;
         isNew = true;
     }
-    public char getLetter() {
+    public String getLetter() {
         return this.letter;
     }
     public int getScore() {
