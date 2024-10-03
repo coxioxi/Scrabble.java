ab6a571045a7eb6e6c5c65e49c4e2bf89d326daf
Ian-Boyer <ianwb90@gmail.com>
2024-10-02 21:24:23 -0400

    board changes

--- code/Board.java
+++ code/Board.java
@@ -2,11 +2,12 @@ import java.lang.reflect.Array;
 
 public class Board {
     public static void main(String[] args) {
-        Array[][] board = new Array[15][15];
+        Tile[][] board = new Tile[15][15];
 
     }
     public Array addToBoard(Array[][] board, Tile letter, int row, int column) {
-        return board[row][column] = letter;
+
+        return null;
     }
 
 }
