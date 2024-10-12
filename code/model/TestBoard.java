3fca700042a530c92f87d426fe3e058c257a1f6b
JyelMason <mason3@otterbein.edu>
2024-10-12 18:03:47 -0400

    prototype now in functioning state with tile object update

--- code/model/TestBoard.java
+++ code/model/TestBoard.java
@@ -10,17 +10,7 @@ public class TestBoard {
     Tile[] tiles = {new Tile('N',new Point(7,7)),new Tile('I',new Point(8,7))
             ,new Tile('C',new Point(9,7)), new Tile('E',new Point(10,7))};
 
-    @Test
-    public void testScore() throws InvalidPositionException {
-        Tile[] tiles = {new Tile('N'),new Tile('I'),new Tile('C'), new Tile('E')};
-        Board board = new Board();
 
-        Tile[] tile = new Tile[board.findOrigin(tiles).size()];
-        Set<Tile> tiles2 = board.findOrigin(tile);
-        tiles2.toArray();
-
-        System.out.println(board.score((Tile[]) tiles2.toArray()));
-    }
 
     //valid points
     Board board = new Board();
