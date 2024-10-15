b2988c8c423fc74e3c8b5955048007d23714463d
dcarr <dcarrfromafar@gmail.com>
2024-10-15 13:29:20 -0400

    updated model for challenges; added documentation

--- code/model/LocalPlayer.java
+++ code/model/LocalPlayer.java
@@ -2,12 +2,20 @@ package model;
 
 /**
  * this class represents the player who is on this computer
+ * it maintains the tiles which the player has access to
+ * it permits plays to be made to update this rack, and plays
+ * to be undone as is the case for failed challenges
  */
-public class LocalPlayer extends Player{
-	private Tile[] rack; 	// the tiles the player has
+public class LocalPlayer extends Player {
+	private Tile[] rack, 		// The tiles the player has.
+			   lastPlay, 		// The tiles which were played on the previous turn
+								// this field is used to undo plays for failed challenges.
+			   newTiles;		// The tiles which have most recently been added to the rack;
+								// these will need to be removed in the case of a failed challenge.
 
 	/**
-	 * constructs a localPlayer object
+	 * constructs a localPlayer object from their name and ID, and
+	 * sets their rack
 	 * @param name the name of the player. Must be at least three characters
 	 * @param ID the player's id, which corresponds to their order
 	 * @param rack the tiles the player has
@@ -18,23 +26,60 @@ public class LocalPlayer extends Player{
 	}
 
 	/**
-	 *
-	 * @param name
-	 * @param ID
+	 * Provides access to the tiles contained in rack
+	 * without the ability to change rack
+	 * @return a cloned copy of rack, the tiles of this player
 	 */
-	public LocalPlayer(String name, int ID) {
-		super(name, ID);
-	}
-
 	public Tile[] getRack() {
-		return rack;
+		return rack.clone();
 	}
 
-	public void removeTiles(Tile[] tiles) {
+	/**
+	 * Updates the player's information after they have
+	 * played specified tiles from their rack and added new ones to it
+	 * @param toPlay the tiles which the player plays and no longer has.
+	 *               Must be non-empty. All tiles in toPlay must have
+	 *               letter values which correspond to tiles in the
+	 *               player's rack
+	 * @param addToRack the tiles which will be added to the rack after
+	 *                  the played tiles are removed.
+	 *                  this array must be the same size or smaller than toPlay.
+	 */
+	public void playTiles(Tile[] toPlay, Tile[] addToRack)
+			throws NoTileFoundException {
+		//TODO: Implement playTiles
+
+		/*
+		Implementation notes/details:
+			for each tile in toPlay, get letter and is blank.
+			see if a matching tile exists in rack. Remove or throw exception.
+			remember to add this tile to lastPlay
+			This process may be easier if rack is converted to an ArrayList,
+			but this option is left to the developer
+
+			add tiles from second array to the rack AFTER above.
+			remember to add these tiles to newTiles
+		 */
 
 	}
 
-	public void addTiles(Tile[] tiles) {
+	/**
+	 * Resets the state of rack to before the most recent play.
+	 * if this method is called next after playTiles,
+	 * this.getRack() will contain all the same tiles
+	 * before call to playTiles. Ordering is not enforced
+	 */
+	public void undoLastPlay() {
+		//TODO: implement
 
+		/*
+		Implementation notes/details:
+			Remove newTiles from rack
+			add lastPlay to rack
+		 */
+	}
+
+	public Tile[] getLastPlay() {
+		return lastPlay;
 	}
 }
--- code/model/Player.java
+++ code/model/Player.java
@@ -2,6 +2,10 @@ package model;
 
 /**
  * Generalized representation of a Player.
+ * Players have a name, and ID, a score, and fields for
+ * ability to make plays and whether they passed their previous turn.
+ * Clients can change the score of the player and set their
+ * activity status and their behavior on their previous turn.
  */
 public class Player {
 	public static int DEFAULT_SCORE = 0;	// the starting score
@@ -39,30 +43,64 @@ public class Player {
 		return name;
 	}
 
+	/**
+	 * getter for score
+	 * @return the player's score as an integer
+	 */
 	public int getScore() {
 		return score;
 	}
 
-	public void setScore(int score) {
-		this.score = score;
+	/**
+	 * Increases the player's score by a specified amount.
+	 * note that score may be decreased by passing a negative number
+	 * no limits are imposed on the score field. Use this method responsibly
+	 * @param amount the amount by which to increase this player's
+	 *               score. Must be between the negative and positive
+	 *               limits of int.
+	 */
+	public void increaseScore(int amount) {
+		this.score += amount;
 	}
 
+	/**
+	 * getter for ID
+	 * @return the player ID, which is unique and represents the order
+	 * in which they play.
+	 */
 	public int getID() {
 		return ID;
 	}
 
+	/**
+	 * getter for hasPassedLastTurn.
+	 * @return boolean value of the field
+	 */
 	public boolean isHasPassedLastTurn() {
 		return hasPassedLastTurn;
 	}
 
+	/**
+	 * changes the value of hasPassedLastTurn.
+	 * @param hasPassedLastTurn the new value of this object's field
+	 */
 	public void setHasPassedLastTurn(boolean hasPassedLastTurn) {
 		this.hasPassedLastTurn = hasPassedLastTurn;
 	}
 
+	/**
+	 * getter for isActive, the ability of the player to make plays
+	 * @return the boolean value of isActive. True if they can make plays,
+	 * false otherwise
+	 */
 	public boolean isActive() {
 		return isActive;
 	}
 
+	/**
+	 * setter for isActive, the ability of the player to make plays
+	 * @param active the new value of this object's field
+	 */
 	public void setActive(boolean active) {
 		isActive = active;
 	}
