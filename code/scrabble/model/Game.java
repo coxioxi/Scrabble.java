62b9c3e327b66582672c5199a1923dacbdc80923
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-18 01:09:56 -0400

    Added documentation, isGameOver, couple more methods

--- code/scrabble/model/Game.java
+++ code/scrabble/model/Game.java
@@ -14,6 +14,14 @@ import java.awt.*;
  * It is responsible for modeling the game and all the components.
  */
 public class Game {
+
+	/*
+	TODO:
+		Jy'el: please consider end conditions as you implement this class's methods
+		I have just added the isGameOver field, as I failed to consider it previously
+
+	 */
+
 	// the players of the game. The order is significant and represents the order in
 	// which turns are taken
 	private Player[] players;
@@ -25,6 +33,7 @@ public class Game {
 	private int gameTime;			// how much time (in seconds) remains in the game
 	private int currentPlayer;		// whose turn it is. Their ID and
 									//  	the player at players[currentPlayer]
+	private boolean isGameOver;		// whether the game has met any end conditions
 
 	public Game(Player[] players, Board board, Ruleset ruleset, Player me){
 		this.players = players;
@@ -38,12 +47,43 @@ public class Game {
 	 * Plays tiles on the board for a player
 	 * @param playerID the ID of the player for whom to make a play
 	 * @param tiles the tiles to be placed. must have at least on tile
-	 * @throws InvalidPositionException if the tiles are not placed correctly.
 	 * see Board.playTiles()
 	 */
-	public void playTiles(int playerID, Tile[] tiles)
-			throws InvalidPositionException {
-		int score = board.playTiles(tiles);
+	public boolean playTiles(int playerID, Tile[] tiles) {
+		Player player = getPlayer(playerID);
+		if (player.isActive()) {
+			int score = board.playTiles(tiles);
+			if (score < 0) return false;
+			player.increaseScore(score);
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/**
+	 * This method will initiate a challenge on the
+	 * most recent turn from the local player.
+	 * The challenge will fail if any word from the turn
+	 * is not in the game dictionary.
+	 * The game object updates the Player and Board objects
+	 * according to the result of the challenge
+	 *
+	 * @return true if the challenge passes.
+	 * 			false if it fails
+	 */
+	public boolean challenge() {
+
+		return true;
+	}
+
+	/*
+	this helper method changes whose turn it is to the next
+	person in the turn list.
+ 	*/
+	private void nextTurn() {
+		this.currentPlayer = (this.currentPlayer + 1) % this.players.length;
 	}
 
 	public int getCurrentPlayerTime() {
@@ -70,17 +110,50 @@ public class Game {
 		this.currentPlayer = currentPlayer;
 	}
 
+	/**
+	 * Increases the score of the specified player by the specified amount.
+	 * Note that the score can be decreased by passing a negative number
+	 * @param playerID the ID of the player for whom to increase the score
+	 * @param amount by which to increase player score
+	 */
 	public void increaseScore(int playerID, int amount) {
+		Player player = getPlayer(playerID);
+		player.increaseScore(amount);
+	}
 
+	/*
+	this helper method returns a reference to the player object
+	whose ID is equal to the parameter
+ 	*/
+	private Player getPlayer(int playerID) {
+		// TODO: implement
+		return null;
 	}
 
 
+	/**
+	 * Passes the turn from this player to the next player
+	 * This method updates the player's status to inactive if they
+	 * have passed their previous turn, otherwise only their
+	 * passedLastTurn field is updated.
+	 * The current player changes to the next player in the order
+	 * @param ID The ID of the player for whom to pass a turn
+	 *           This ID must correspond to the ID of the current player
+	 */
 	public void passTurn(int ID) {
-
+		// TODO: implement
 	}
 
+	/**
+	 * Changes the status of the player's connection
+	 * Note this change should only be made for NetworkPlayers,
+	 * and not for the local player. the latter will cause errors
+	 * @param playerID the player to change the status of
+	 * @param isConnected The new status of the player
+	 */
 	public void setConnected(int playerID, boolean isConnected) {
-
+		// TODO: implement
+		// also change isactive if isconnected is false
 	}
 
 	public boolean isConnected(int playerID) {
@@ -91,10 +164,22 @@ public class Game {
 		return true;
 	}
 
+	/**
+	 * Changes the ability of the specified player to make plays
+	 * @param playerID the player for whom to change activity status
+	 * @param isActive the status of the player
+	 */
 	public void setActive(int playerID, boolean isActive) {
 
 	}
 
+	/**
+	 * changes the rack size of specified player.
+	 * This method should only be called on NetworkPlayers
+	 * @param playerID the player for whom to change the rack size.
+	 * @param amount the amount to change the rack size by. Note that
+	 *               the total size of a player's rack should never exceed 7.
+	 */
 	public void decreaseRack(int playerID, int amount) {
 
 	}
@@ -103,14 +188,32 @@ public class Game {
 		return 0;
 	}
 
+	/**
+	 * Adds tiles to the rack of the local player.
+	 * the number of tiles added must not overfill the rack
+	 *
+	 * @param tiles the tiles to add to the player's rack
+	 *              tiles.length should be equal to that of the
+	 *              most recent call to Board.playTiles() when
+	 *              playerID == LocalPlayer.ID
+	 */
 	public void addTiles(Tile[] tiles) {
 
 	}
 
+	/**
+	 * Removes tiles from the LocalPlayer (necessary in the case of
+	 * a failed challenge)
+	 * @param tiles the tiles to remove. length <= 7
+	 */
 	public void removeTiles(Tile[] tiles) {
 
 	}
 
+	/**
+	 * Gets the tiles of the local player
+	 * @return the tiles of the local player
+	 */
 	public Tile[] getPlayerTiles() {
 		return self.getRack();
 	}
