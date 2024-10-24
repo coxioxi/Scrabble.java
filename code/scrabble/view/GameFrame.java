2c182600fb1dee9e809e7af37d5949bd172d31dd
Moose Lat <mooselat12@gmail.com>
2024-10-24 01:14:46 -0400

    Changes to layout of code in hostScreen for readability. Made changes to joinScreen to make the layout more readable and better looking

--- code/scrabble/view/GameFrame.java
+++ code/scrabble/view/GameFrame.java
@@ -77,65 +77,68 @@ public class GameFrame extends JFrame {
 
 
     public static void hostScreen() {
+        // Frame setup
         JFrame hostFrame = new JFrame("Scrabble");
         hostFrame.setSize(800,600);
-        //hostFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         hostFrame.setLayout(null);
+
+        // Main Panel setup
         JPanel mainPanel = new JPanel();
         mainPanel.setBounds(0,0,800,600);
         mainPanel.setLayout(null);
 
-
+        // Page Title
         JLabel hostTitle = new JLabel("Host Game", SwingConstants.CENTER);
         hostTitle.setFont(titleFont);
         hostTitle.setBounds(275,5,250,45);
         mainPanel.add(hostTitle);
 
         // Main Frame details
+        // IP Address Labels setup
         JLabel yourIP = new JLabel("Your IP Address:");
-        JLabel nameLabel = new JLabel("Name:");
+        yourIP.setFont(labelFont);
+        yourIP.setBounds(25, 80, 240, 30);
+
         JLabel hostsIP = new JLabel("**Host's IP**");
+        hostsIP.setFont(labelFont);
+        hostsIP.setBounds(245, 80, 200, 30);
+
+        // Name Label and TextField setup
+        JLabel nameLabel = new JLabel("Name:");
+        nameLabel.setFont(labelFont);
+        nameLabel.setBounds(25, 150, 100, 30);
+
         JTextField nameInput = new JTextField();
+        nameInput.setFont(labelFont);
+        nameInput.setBounds(115, 150, 220, 30);
+
+        // Host Button setup
         JButton hostButton = new JButton("Host");
+        hostButton.setFont(labelFont);
+        hostButton.setBounds(325, 490, 150, 40);
+
+        // Player Waiting Label setup
         JLabel waitingLabel = new JLabel("Players Waiting");
-        JLabel player1Waiting = new JLabel("**Player 1 Name**");
-        JLabel player2Waiting = new JLabel("**Player 2 Name**");
-        JLabel player3Waiting = new JLabel("**Player 3 Name**");
-        JLabel player4Waiting = new JLabel("**Player 4 Name**");
+        waitingLabel.setFont(labelFont);
+        waitingLabel.setBounds(50, 215, 200, 30);
+
+        // Player Names Waiting labels setup
+        JLabel player1Waiting = new JLabel("**Player 1 Name**", SwingConstants.CENTER);
+        JLabel player2Waiting = new JLabel("**Player 2 Name**", SwingConstants.CENTER);
+        JLabel player3Waiting = new JLabel("**Player 3 Name**", SwingConstants.CENTER);
+        JLabel player4Waiting = new JLabel("**Player 4 Name**", SwingConstants.CENTER);
 
         Font waitingFont = new Font("Arial", Font.PLAIN, 20);
-
-        yourIP.setFont(labelFont);
-        nameLabel.setFont(labelFont);
-        hostsIP.setFont(labelFont);
-        nameInput.setFont(labelFont);
-        hostButton.setFont(labelFont);
-        waitingLabel.setFont(labelFont);
         player1Waiting.setFont(waitingFont);
         player2Waiting.setFont(waitingFont);
         player3Waiting.setFont(waitingFont);
         player4Waiting.setFont(waitingFont);
-
-        player1Waiting.setHorizontalAlignment(SwingConstants.CENTER);
-        player1Waiting.setVerticalAlignment(SwingConstants.CENTER);
-        player2Waiting.setHorizontalAlignment(SwingConstants.CENTER);
-        player2Waiting.setVerticalAlignment(SwingConstants.CENTER);
-        player3Waiting.setHorizontalAlignment(SwingConstants.CENTER);
-        player3Waiting.setVerticalAlignment(SwingConstants.CENTER);
-        player4Waiting.setHorizontalAlignment(SwingConstants.CENTER);
-        player4Waiting.setVerticalAlignment(SwingConstants.CENTER);
 
         player1Waiting.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         player2Waiting.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         player3Waiting.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
         player4Waiting.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
-        yourIP.setBounds(25, 80, 240, 40);
-        nameLabel.setBounds(25, 150, 100, 40);
-        hostsIP.setBounds(245, 80, 200, 40);
-        nameInput.setBounds(115, 150, 225, 40);
-        hostButton.setBounds(325, 490, 150, 50);
-        waitingLabel.setBounds(50, 215, 200, 40);
         player1Waiting.setBounds(25, 270, 250, 40);
         player2Waiting.setBounds(25, 330, 250, 40);
         player3Waiting.setBounds(25, 390, 250, 40);
@@ -147,44 +150,54 @@ public class GameFrame extends JFrame {
         customizations.setBorder(new LineBorder(Color.BLACK, 2));
         customizations.setLayout(null);
 
+        Font customizationFont = new Font("Arial", Font.PLAIN, 16);
+
+        // Customizations Label setup
         JLabel customizationLabel = new JLabel("Customizations");
+        customizationLabel.setFont(labelFont);
+        customizationLabel.setBounds(50, 10, 200, 30);
+
+        // Challenge Label and Combo Box setup
         JLabel challengeLabel = new JLabel("Challenges Allowed:");
-        JLabel dictionaryLabel = new JLabel("Dictionary Used:");
-        JLabel playerTimeLabel = new JLabel("Player Time:");
-        JLabel gameTimeLabel = new JLabel("Game Time:");
+        challengeLabel.setFont(customizationFont);
+        challengeLabel.setBounds(10, 80, 150, 20);
 
         String[] challengeChoices = {"Challenges on", "Challenges off"};
         JComboBox<String> challengeComboBox = new JComboBox<>(challengeChoices);
+        challengeComboBox.setFont(customizationFont);
+        challengeComboBox.setBounds(160, 78, 130, 25);
+
+        // Dictionary Label and Combo Box setup
+        JLabel dictionaryLabel = new JLabel("Dictionary Used:");
+        dictionaryLabel.setFont(customizationFont);
+        dictionaryLabel.setBounds(10, 120, 150, 20);
+
         String[] dictionaryChoices = {"Dictionary 1", "Dictionary 2"};
         JComboBox<String> dictionaryComboBox = new JComboBox<>(dictionaryChoices);
+        dictionaryComboBox.setFont(customizationFont);
+        dictionaryComboBox.setBounds(133, 118, 120, 25);
+
+        // Player Time Label and Combo Box setup
+        JLabel playerTimeLabel = new JLabel("Player Time:");
+        playerTimeLabel.setFont(customizationFont);
+        playerTimeLabel.setBounds(10, 160, 150, 20);
+
         String[] playerTimeChoices = {"2 Minutes", "3 Minutes", "4 Minutes", "5 Minutes"};
         JComboBox<String> playerTimeComboBox = new JComboBox<>(playerTimeChoices);
+        playerTimeComboBox.setFont(customizationFont);
+        playerTimeComboBox.setBounds(105, 158, 105, 25);
+
+        // Game Time Label and Combo Box setup
+        JLabel gameTimeLabel = new JLabel("Game Time:");
+        gameTimeLabel.setFont(customizationFont);
+        gameTimeLabel.setBounds(10, 200, 150, 20);
+
         String[] gameTimeChoices = {"30 Minutes", "45 Minutes", "60 Minutes", "75 Minutes"};
         JComboBox<String> gameTimeComboBox = new JComboBox<>(gameTimeChoices);
-
-        Font customizationFont = new Font("Arial", Font.PLAIN, 16);
-
-        customizationLabel.setFont(labelFont);
-        challengeLabel.setFont(customizationFont);
-        dictionaryLabel.setFont(customizationFont);
-        playerTimeLabel.setFont(customizationFont);
-        gameTimeLabel.setFont(customizationFont);
-        challengeComboBox.setFont(customizationFont);
-        dictionaryComboBox.setFont(customizationFont);
-        playerTimeComboBox.setFont(customizationFont);
         gameTimeComboBox.setFont(customizationFont);
-
-        customizationLabel.setBounds(50, 10, 200, 30);
-        challengeLabel.setBounds(10, 80, 150, 20);
-        dictionaryLabel.setBounds(10, 120, 150, 20);
-        playerTimeLabel.setBounds(10, 160, 150, 20);
-        gameTimeLabel.setBounds(10, 200, 150, 20);
-        challengeComboBox.setBounds(160, 78, 130, 25);
-        dictionaryComboBox.setBounds(133, 118, 120, 25);
-        playerTimeComboBox.setBounds(105, 158, 105, 25);
         gameTimeComboBox.setBounds(105, 198, 105, 25);
 
-        // adds items to the main frame
+        // adds items to the main panel
         mainPanel.add(yourIP);
         mainPanel.add(nameLabel);
         mainPanel.add(hostsIP);
@@ -211,6 +224,7 @@ public class GameFrame extends JFrame {
 
         hostFrame.setVisible(true);
 
+        // Changes the "X" in the top right to make the Host Screen return to the Main Menu
         hostFrame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -224,54 +238,56 @@ public class GameFrame extends JFrame {
         // Highest level frame and panel
         JFrame joinFrame = new JFrame("Scrabble");
         joinFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        joinFrame.setSize(800, 800);
+        joinFrame.setSize(700, 500);
         joinFrame.setLayout(null);
 
         // Add title at the top of the frame
         JLabel joinTitle = new JLabel("Player Joining Screen", SwingConstants.CENTER);
         joinTitle.setFont(titleFont);
-        joinTitle.setBounds(150, 30, 500, 50);
+        joinTitle.setBounds(160, 5, 380, 50);
         joinFrame.add(joinTitle);
 
         // Name label and text field
         JLabel nameLabel = new JLabel("Name:");
-        nameLabel.setBounds(150, 100, 100, 40);
+        nameLabel.setBounds(190, 80, 80, 30);
         nameLabel.setFont(labelFont);
         joinFrame.add(nameLabel);
 
         JTextField enterName = new JTextField();
-        enterName.setBounds(250, 100, 200, 40);
+        enterName.setBounds(290, 75, 220, 40);
+        enterName.setFont(labelFont);
         joinFrame.add(enterName);
 
         // Panel for border with Host IP text field and Join button inside
         JPanel borderPanel = new JPanel();
         borderPanel.setBorder(new LineBorder(Color.BLACK, 2));
-        borderPanel.setBounds(150, 200, 500, 500);
+        borderPanel.setBounds(125, 140, 450, 300);
         borderPanel.setLayout(null);
         joinFrame.add(borderPanel);
 
         // Labels for Join party and Host IP inside bordered box
         JLabel joinPartyLabel = new JLabel("Join Party", SwingConstants.CENTER);
-        joinPartyLabel.setBounds(160, 20, 180, 30);
+        joinPartyLabel.setBounds(135, 15, 180, 30);
         joinPartyLabel.setFont(labelFont);
         borderPanel.add(joinPartyLabel);
 
         JLabel enterIPLabel = new JLabel("Enter Host IP:");
-        enterIPLabel.setBounds(40, 80, 180, 30);
+        enterIPLabel.setBounds(30, 80, 180, 30);
         enterIPLabel.setFont(labelFont);
         borderPanel.add(enterIPLabel);
 
         // Text field for entering Host IP
         JTextField hostIPText = new JTextField();
-        hostIPText.setBounds(240, 80, 200, 30);
+        hostIPText.setBounds(220, 75, 200, 40);
         borderPanel.add(hostIPText);
 
         // Join button
         JButton joinButton = new JButton("Join");
-        joinButton.setBounds(150, 150, 200, 40);
+        joinButton.setBounds(150, 150, 150, 50);
         joinButton.setFont(labelFont);
         borderPanel.add(joinButton);
 
+        // Changes the "X" in the top right to make the Join Screen return to the Main Menu
         joinFrame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
