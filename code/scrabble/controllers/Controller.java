3e2857573ab9d81760ceff62d4189d75c4999d89
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-25 15:51:35 -0400

    Talked about responsibilities of Controller

---
+++ code/scrabble/controllers/Controller.java
@@ -0,0 +1,14 @@
+package scrabble.controllers;
+
+
+/**
+ * Run the other classes
+ */
+public class Controller {
+
+	// Runs View and Game.
+
+	// Main is super important
+
+	// Controller creates the view and adds listeners
+}
