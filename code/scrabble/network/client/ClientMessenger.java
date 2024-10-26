9f0115b40871cfbf7d87ba4b87ec05835c41bad7
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-25 21:31:25 -0400

    Added documentation

--- code/scrabble/network/client/ClientMessenger.java
+++ code/scrabble/network/client/ClientMessenger.java
@@ -1,5 +1,13 @@
 package scrabble.network.client;
 
-
+/**
+ * This class is responsible for sending and receiving messages from the host
+ * after a socket has already been established.
+ * ClientMessenger uses PropertyChangeSupport to notify the controller class
+ * when new messages are received.
+ */
 public class ClientMessenger {
+	// Use ObjectOutputStream and ObjectInputStream for sending and receiving messages
+	// Pass in a socket to the host, get input and output streams from socket,
+	// then create OOS and OIS.
 }
