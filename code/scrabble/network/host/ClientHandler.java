cd2c77b9f36e0aa7c68715b3aca6ddc5f3024836
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-25 21:40:22 -0400

    Added documentation

--- code/scrabble/network/host/ClientHandler.java
+++ code/scrabble/network/host/ClientHandler.java
@@ -1,4 +1,24 @@
 package scrabble.network.host;
 
-public class ClientHandler {
+/**
+ * ClientHandler is responsible for listening for new messages coming in from the clients
+ * It maintains a reference to the PartyHost to notify it when messages are received.
+ * For example,
+ * <code>
+ * 		Message message = inputStream.readUTF();
+ * 		partyHost.sendMessage(message)
+ * 	</code>
+ * The party host is then responsible for handling the message that was received.
+ * Party host creates one ClientHandler thread for each client who joins the game
+ *
+ */
+public class ClientHandler  implements Runnable {
+	/*
+	The majority of the work for this class will be handled inside the run() method, with
+	potentially helper methods being called.
+	 */
+	@Override
+	public void run() {
+
+	}
 }
