f10c8e918fd29d0233445edf0537e5a67bc31b46
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-26 03:39:45 -0400

    Refactoring

--- code/scrabble/network/messages/Message.java
+++ code/scrabble/network/messages/Message.java
@@ -3,7 +3,8 @@ package scrabble.network.messages;
 import java.io.Serializable;
 
 /**
- see <a href="https://docs.oracle.com/javase/8/docs/api/java/io/Serializable.html"></a>
+ see <a href="https://docs.oracle.com/javase/8/docs/api/java/io/Serializable.html">
+ 	Serializable Documentation</a>
  */
 public abstract class Message implements Serializable {
 }
