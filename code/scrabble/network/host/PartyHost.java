e98d4070e4fa2a91c83e662367a3e57fc7c569ab
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-25 21:51:08 -0400

    Added documentation

--- code/scrabble/network/host/PartyHost.java
+++ code/scrabble/network/host/PartyHost.java
@@ -1,4 +1,30 @@
 package scrabble.network.host;
 
-public class PartyHost {
+/*
+this class is the server side
+Usage: 	javac scrabble/network/host/PartyHost.java
+		java scrabble/network/host/PartyHost
+		Use ../controllers/NetworkController as client
+David: cd "OneDrive - Otterbein University\IdeaProjects\Scrabble\code"
+
+ */
+
+/**
+ * PartyHost receives messages from clients (via ClientHandler) and sends them to clients
+ * This class processes messages as needed depending on the type
+ */
+public class PartyHost implements Runnable {
+	/*
+	Some message processing is likely to be needed depending on the messages received.
+	For example, when a PlayTiles message is received, the host must send a NewTiles message
+	to the client who sent the PlayTiles. Host then must send PlayTiles to the other
+	clients, with the new number of tiles which the original client has in their rack.
+
+	It seems reasonable to me (david) to have helper methods for each individual type of method and
+	call it using a switch or if-else-if structure based on the type of class
+	 */
+	@Override
+	public void run() {
+
+	}
 }
