72ac8ac205c7ac94f6d9e6ca4e994ce2491ba063
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-25 23:44:35 -0400

    Added catch for EOFException

--- code/scrabble/network/host/experimenting/Client.java
+++ code/scrabble/network/host/experimenting/Client.java
@@ -4,14 +4,15 @@ import java.io.IOException;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Objects;
 import java.util.Scanner;
 /*
 cd "OneDrive - Otterbein University/IdeaProjects/Scrabble/code"
-javac ./scrabble/network/host/experimenting/Client.java
-java ./scrabble/network/host/experimenting/Client
+javac scrabble/network/host/experimenting/Client.java
+java scrabble/network/host/experimenting/Client 10.80.2.1 5000
  */
 public class Client {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		String address = args[0];
 		int port = Integer.parseInt(args[1]);
 		Socket socket = new Socket(address, port);
@@ -20,8 +21,17 @@ public class Client {
 		System.out.println("Enter message: ");
 		String message = input.next();
 		ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+		while (!Objects.equals(message.toLowerCase(), "quit")) {
+			outputStream.writeObject(message);
+			outputStream.flush();
+			System.out.println("Enter message: ");
+			message = input.nextLine();
+		}
+
 		outputStream.writeObject(message);
 		outputStream.flush();
+		Thread.sleep(1000);
 		outputStream.close();
+		input.close();
 	}
 }
