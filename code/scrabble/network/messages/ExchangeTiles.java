b88e7fcb94873b0d0f91ad2602b8ad1d53f1ff61
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-27 00:45:35 -0400

    serializing Tile arrays

--- code/scrabble/network/messages/ExchangeTiles.java
+++ code/scrabble/network/messages/ExchangeTiles.java
@@ -1,14 +1,20 @@
 package scrabble.network.messages;
 
+import scrabble.model.NotBlankException;
 import scrabble.model.Tile;
 
+import java.awt.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serial;
+import java.util.ArrayList;
 
 public class ExchangeTiles extends Message{
 	@Serial
 	private static final long serialVersionUID = 3L;
 	private final int playerID;
-	private final Tile[] toExchange;
+	private Tile[] toExchange;
 
 	public ExchangeTiles(int senderID, int playerID, Tile[] toExchange) {
 		super(senderID);
@@ -24,5 +30,18 @@ public class ExchangeTiles extends Message{
 		return toExchange;
 	}
 
+	@Serial
+	private void writeObject(ObjectOutputStream out)
+			throws IOException {
+		out.defaultWriteObject();
+		writeTiles(out, toExchange);
+	}
+
+	@Serial
+	private void readObject(ObjectInputStream in)
+			throws IOException, ClassNotFoundException {
+		in.defaultReadObject();
+		toExchange = readTile(in);
+	}
 
 }
--- code/scrabble/network/messages/Message.java
+++ code/scrabble/network/messages/Message.java
@@ -1,7 +1,11 @@
 package scrabble.network.messages;
 
-import java.io.Serial;
-import java.io.Serializable;
+import scrabble.model.NotBlankException;
+import scrabble.model.Tile;
+
+import java.awt.*;
+import java.io.*;
+import java.util.ArrayList;
 
 /**
  see <a href="https://docs.oracle.com/javase/8/docs/api/java/io/Serializable.html">
@@ -21,5 +25,42 @@ public abstract class Message implements Serializable {
 
 	public int getSenderID() {
 		return senderID;
+	}
+
+	protected static void writeTiles(ObjectOutputStream out, Tile[] tiles)
+			throws IOException {
+		for (Tile t: tiles) {
+			out.writeObject(t.getLetter());
+			out.writeObject(t.isBlank());
+			out.writeObject(t.getLocation());
+			out.writeObject(t.getIsNew());
+		}
+	}
+
+	protected static Tile[] readTile(ObjectInputStream in)
+			throws IOException, ClassNotFoundException {
+		ArrayList<Tile> tiles = new ArrayList<>(7);
+		while (in.available() > 0) {
+			char    letter   = (Character) 	in.readObject();
+			boolean isBlank  = (Boolean) 	in.readObject();
+			Point location = (Point) 		in.readObject();
+			boolean isNew 	 = (Boolean) 	in.readObject();
+			Tile tile;
+			if (isBlank) {
+				tile = new Tile();
+				try {
+					tile.setLetter(letter);
+				} catch (NotBlankException e) {
+					System.out.println("LMAO wtf just happened.\nerror in serializing tile");
+				}
+			}
+			else {
+				tile = new Tile(letter);
+			}
+			tile.setLocation(location);
+			tile.setIsNew(isNew);
+			tiles.add(tile);
+		}
+		return tiles.toArray(new Tile[0]);
 	}
 }
