e9c8479b7625ce473a257990ab687c8459968be8
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-10-28 21:03:46 -0400

    debug + add main for running

--- code/scrabble/network/client/ClientMessenger.java
+++ code/scrabble/network/client/ClientMessenger.java
@@ -7,15 +7,16 @@ usage: 	javac scrabble/network/client/ClientMessenger.java
 		../host/PartyHost acts as server and prints out local IP for host address
 		David: cd "OneDrive - Otterbein University\IdeaProjects\Scrabble\code"
  */
-import scrabble.network.messages.Message;
+import scrabble.model.Tile;
+import scrabble.network.messages.*;
 
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.io.EOFException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * This class is responsible for sending and receiving messages from the host
@@ -58,6 +59,13 @@ public class ClientMessenger implements Runnable {
 					inputStream.close();
 					isClosed = true;
 				}
+			} catch (StreamCorruptedException e) {
+				System.out.println("Stream corrupted: " + e);
+				try {
+					inputStream = new ObjectInputStream(server.getInputStream());
+				} catch (IOException ex) {
+					throw new RuntimeException(ex);
+				}
 			} catch (ClassNotFoundException | IOException e) {
 				throw new RuntimeException(e);
 			}
@@ -77,5 +85,95 @@ public class ClientMessenger implements Runnable {
 		outputStream.close();
 		inputStream.close();
 		server.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter host IP: ");
+		String host = in.next();
+		System.out.print("Enter port: ");
+		int port = in.nextInt();
+
+		Socket socket = null;
+		try {
+			socket = new Socket(host, port);
+		}
+		catch (IOException e) {
+			System.out.print("Error in socket: " + e);
+			throw new RuntimeException(e);
+		}
+
+
+		ClientMessenger clientMessenger = new ClientMessenger(socket, evt -> {
+			try {
+				((ClientMessenger) evt.getSource()).sendMessage((Message)evt.getNewValue());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		});
+
+		Thread thread = new Thread(clientMessenger);
+		thread.start();
+
+		System.out.println("Message type (challenge, exchange, exit, newTiles, pass,\n\tplay, start): ");
+		String messageType = in.next();
+
+		if (messageType.equalsIgnoreCase("challenge")) {
+			System.out.print("Enter senderID, challengingPlayerID, challengedPlayerID: ");
+			int sID, cpID, cdpID;
+			sID = in.nextInt();
+			cpID = in.nextInt();
+			cdpID = in.nextInt();
+			clientMessenger.sendMessage(new Challenge(sID, cpID, cdpID));
+		} else if (messageType.equalsIgnoreCase("exchange")) {
+			System.out.print("Enter senderID, playerID: ");
+			int sID, pID;
+			sID = in.nextInt();
+			pID = in.nextInt();
+			System.out.println("Enter tile letter, x and y: ");
+			int tL, x, y;
+			tL = in.next().charAt(0);
+			x = in.nextInt();
+			y = in.nextInt();
+			clientMessenger.sendMessage(new ExchangeTiles(sID, pID,
+					new Tile[]{new Tile((char)tL, new Point(x, y))}));
+		} else if (messageType.equalsIgnoreCase("exit")) {
+			System.out.print(" sender, player: ");
+			int sID, pID;
+			sID = in.nextInt();
+			pID = in.nextInt();
+			clientMessenger.sendMessage(new ExitParty(sID, pID));
+		} else if (messageType.equalsIgnoreCase("newTiles")) {
+			System.out.print("Enter senderID: ");
+			int sID = in.nextInt();
+			System.out.println("Enter tile letter, x and y: ");
+			int tL, x, y;
+			tL = in.next().charAt(0);
+			x = in.nextInt();
+			y = in.nextInt();
+			clientMessenger.sendMessage(new NewTiles(sID,
+					new Tile[]{new Tile((char)tL, new Point(x, y))}));
+		} else if (messageType.equalsIgnoreCase("pass")) {
+			System.out.print(" sender, player: ");
+			int sID, pID;
+			sID = in.nextInt();
+			pID = in.nextInt();
+			clientMessenger.sendMessage(new PassTurn(sID, pID));
+		} else if (messageType.equalsIgnoreCase("play")) {
+			System.out.print("Enter senderID, playerID: ");
+			int sID, pID;
+			sID = in.nextInt();
+			pID = in.nextInt();
+			System.out.println("Enter tile letter, x and y: ");
+			int tL, x, y;
+			tL = in.next().charAt(0);
+			x = in.nextInt();
+			y = in.nextInt();
+			clientMessenger.sendMessage(new PlayTiles(sID, pID,
+					new Tile[]{new Tile((char)tL, new Point(x, y))}));
+		} else if (messageType.equalsIgnoreCase("start")) {
+			System.out.print("Not implemented");
+		}
+
 	}
 }
