b195cc0d40865e1791802a6cbf2710603e4471c0
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-11-01 17:09:53 -0400

    implemented execute

--- code/scrabble/network/messages/ExchangeTiles.java
+++ code/scrabble/network/messages/ExchangeTiles.java
@@ -1,5 +1,6 @@
 package scrabble.network.messages;
 
+import scrabble.controller.Controller;
 import scrabble.model.NotBlankException;
 import scrabble.model.Tile;
 
@@ -7,6 +8,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serial;
+import java.util.ArrayList;
 
 public class ExchangeTiles extends Message{
 	@Serial
@@ -28,18 +30,36 @@ public class ExchangeTiles extends Message{
 		return toExchange;
 	}
 
-	@Serial
-	private void writeObject(ObjectOutputStream out)
-			throws IOException {
-		out.defaultWriteObject();
-		writeTiles(out, toExchange);
+	@Override
+	public void execute(Controller controller) {
+
+		if (playerID == controller.getModel().getSelf().getID()) {
+			selfExecute(controller);
+		}
+		else {
+			themExecute(controller);
+		}
+
+		controller.getModel().nextTurn();
 	}
 
-	@Serial
-	private void readObject(ObjectInputStream in)
-			throws IOException, ClassNotFoundException {
-		in.defaultReadObject();
-		toExchange = readTile(in);
+	private void themExecute(Controller controller) {
+
 	}
+
+	private void selfExecute(Controller controller) {
+		try {
+			controller.getMessenger().sendMessage(this);
+		} catch (IOException e) {
+			// if an error occurs in writing to the host, the host has disconnected.
+			// halt the messenger thread and notify the client that the game is over.
+		}
+		ArrayList<Tile> tiles = controller.getModel().getSelf().getRack();
+		for (Tile t : toExchange) {
+			tiles.remove(t);
+		}
+		// TODO: update the GUI
+	}
+
 
 }
