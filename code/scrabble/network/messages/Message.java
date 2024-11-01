ca2c40217a237862e20564f04a84f388aa29c81e
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-11-01 16:29:02 -0400

    abstract Execute method added

--- code/scrabble/network/messages/Message.java
+++ code/scrabble/network/messages/Message.java
@@ -1,5 +1,6 @@
 package scrabble.network.messages;
 
+import scrabble.controller.Controller;
 import scrabble.model.NotBlankException;
 import scrabble.model.Tile;
 
@@ -43,40 +44,5 @@ public abstract class Message implements Serializable {
 		return senderID;
 	}
 
-	protected static void writeTiles(ObjectOutputStream out, Tile[] tiles)
-			throws IOException {
-		for (Tile t: tiles) {
-			out.writeObject(t.getLetter());
-			out.writeObject(t.isBlank());
-			out.writeObject(t.getLocation());
-			out.writeObject(t.getIsNew());
-		}
-	}
-
-	protected static Tile[] readTile(ObjectInputStream in)
-			throws IOException, ClassNotFoundException {
-		ArrayList<Tile> tiles = new ArrayList<>(7);
-		while (in.available() > 0) {
-			char    letter   = (Character) 	in.readObject();
-			boolean isBlank  = (Boolean) 	in.readObject();
-			Point location = (Point) 		in.readObject();
-			boolean isNew 	 = (Boolean) 	in.readObject();
-			Tile tile;
-			if (isBlank) {
-				tile = new Tile();
-				try {
-					tile.setLetter(letter);
-				} catch (NotBlankException e) {
-					System.out.println("LMAO wtf just happened.\nerror in serializing tile");
-				}
-			}
-			else {
-				tile = new Tile(letter);
-			}
-			tile.setLocation(location);
-			tile.setIsNew(isNew);
-			tiles.add(tile);
-		}
-		return tiles.toArray(new Tile[0]);
-	}
+	public abstract void execute(Controller controller);
 }
