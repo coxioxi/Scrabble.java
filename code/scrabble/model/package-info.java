2dcb32d673a28cc3a32afecbe2120c8367c15714
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-11-01 21:29:26 -0400

    package description

---
+++ code/scrabble/model/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ *     Manages essential Scrabble game data structures.
+ * </p>
+ * <p>
+ *     A game of Scrabble is made up of players, tiles, a board, a ruleset, and game information.
+ *     This package maintains these data in classes of the same name, with Game corresponding to game information.
+ *     All modifications to a Scrabble game should be made through the {@link scrabble.model.Game} interface.
+ * </p>
+ */
+package scrabble.model;
