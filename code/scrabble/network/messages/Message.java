5a83cd9db895460b49594ff7000fd413004457f1
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-11-02 11:34:48 -0400

    added execute(PartyHost)

--- code/scrabble/network/messages/Message.java
+++ code/scrabble/network/messages/Message.java
@@ -3,6 +3,7 @@ package scrabble.network.messages;
 import scrabble.controller.Controller;
 import scrabble.model.NotBlankException;
 import scrabble.model.Tile;
+import scrabble.network.host.PartyHost;
 
 import java.awt.*;
 import java.io.*;
@@ -14,7 +15,6 @@ import java.util.ArrayList;
  */
 public abstract class Message implements Serializable {
 
-	public static final int HOST_ID = -1;
 	@Serial
 	private static final long serialVersionUID = 1L;
 
@@ -36,10 +36,18 @@ public abstract class Message implements Serializable {
 		}
 	}
 
+	/**
+	 * Constructs a message with a senderID. Only to be used by subclasses
+	 * @param senderID the ID of the message's sender.
+	 */
 	protected Message(int senderID) {
 		this.senderID = senderID;
 	}
 
+	/**
+	 * Gets the sender ID.
+	 * @return returns the ID of the player who sent the message.
+	 */
 	public int getSenderID() {
 		return senderID;
 	}
@@ -48,10 +56,22 @@ public abstract class Message implements Serializable {
 	 * This method operates on the controller to make changes to the GUI
 	 * and the view based on what type of message this is. For example,
 	 * a play tiles message will update the board model to have the tiles placed on it
-	 * and change the view for the player
+	 * and change the view for the player.
+	 *
 	 * @param controller the controller on which to make changes. Note that this object
 	 *                   must use public getter methods for all the components
 	 *                   (for example, the GUI components, the model components etc.)
 	 */
 	public abstract void execute(Controller controller);
+
+	/**
+	 * This method operates on the <code>host</code> to make changes to the clients
+	 * when necessary. For example, an execution of a <code>PlayTiles</code> message
+	 * would require new tiles to be sent to the sending client and for the original message
+	 * to be relayed to other players.
+	 *
+	 * @param partyHost the <code>PartyHost</code> on which to make changes. Note that this object
+	 *                   must use public getter methods for all the components which should be changed.
+	 */
+	public abstract void execute(PartyHost partyHost);
 }
