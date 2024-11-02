8414649e956ed366a1384c123b077e8dd95bb313
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-11-02 11:35:04 -0400

    updated documentation to reflect package changes

--- code/scrabble/network/messages/package-info.java
+++ code/scrabble/network/messages/package-info.java
@@ -5,10 +5,16 @@
  * <p>
  *     All message objects extend the abstract class {@link scrabble.network.messages.Message}.
  *     The GUI generates Message objects in response to events. Each message object implements
- *     the abstract {@link scrabble.network.messages.Message#execute}
+ *     the abstract {@link scrabble.network.messages.Message#execute(scrabble.controller.Controller)}
  *     method, which operates on the controller.
  *     This execute method updates the game model dynamically; if the player who creates the message
  *     is the same as the application's Local player object, then the message is relayed to the host.
+ * </p>
+ * <p>
+ *     Similarly, the abstract {@link scrabble.network.messages.Message#execute(scrabble.network.host.PartyHost)}
+ *     operates on the <code>PartyHost</code> object. Calling <code>execute</code> makes any
+ *     necessary changes to the host (such as returning tiles, ending the listening thread, etc.),
+ *     then sends to the other clients.
  * </p>
  * <p>
  * @see scrabble.model.LocalPlayer LocalPlayer
