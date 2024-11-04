2c9b4884d73a4fb8503d073af02f9c7c983ca845
samuel <samukaka1000@gmail.com>
2024-11-03 20:46:01 -0800

    Creating controller submit action listener

--- code/scrabble/controller/Controller.java
+++ code/scrabble/controller/Controller.java
@@ -3,10 +3,15 @@ package scrabble.controller;
 
 
 import scrabble.model.Game;
+import scrabble.model.Tile;
 import scrabble.network.client.ClientMessenger;
+import scrabble.network.messages.PlayTiles;
 import scrabble.network.networkPrototype.PartyHost;
 import scrabble.view.frame.GameFrame;
+import scrabble.view.panel.GameScreen;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.net.Socket;
@@ -14,8 +19,9 @@ import java.net.Socket;
 /**
  * Run the other classes
  */
-public class Controller implements PropertyChangeListener  {
 
+public class Controller implements PropertyChangeListener  {
+	GameScreen gameScreen = new GameScreen();
 	// Runs View and Game.
 
 	// Main is super important
@@ -35,11 +41,22 @@ public class Controller implements PropertyChangeListener  {
 	 */
 	private PartyHost host;
 
+	public void submitActionListener(){
+		gameScreen.getSubmitButton().addActionListener(new ActionListener() {
 
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				PlayTiles playTiles = new PlayTiles(0,0, gameScreen.getPlayedTiles().toArray(new Tile[0]));
+				playTiles.execute(Controller.this);
+			}
+		});
+	}
 
 	public static void main(String[] args) {
 		new Controller();
 	}
+
+	public Controller() {}
 
 	public GameFrame getView() {
 		return view;
@@ -61,21 +78,11 @@ public class Controller implements PropertyChangeListener  {
 		return host;
 	}
 
-	public Controller() {
+	private void addListeners() {}
 
-	}
-
-	private void addListeners() {
-
-	}
-
-	private void hostGame() {
-
-	}
+	private void hostGame() {}
 
 	@Override
-	public void propertyChange(PropertyChangeEvent evt) {
-
-	}
+	public void propertyChange(PropertyChangeEvent evt) {}
 
 }
