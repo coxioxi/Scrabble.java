92d797a61aa076d57925c4bb7a84ec2f57e820c0
dcarrfromafar <90341408+dcarrfromafar@users.noreply.github.com>
2024-11-05 17:29:03 -0500

    refactor using new panel objects

--- code/scrabble/view/panel/GameScreen.java
+++ code/scrabble/view/panel/GameScreen.java
@@ -1,100 +1,44 @@
 package scrabble.view.panel;
 
-import scrabble.controller.Controller;
 import scrabble.model.Board;
 import scrabble.model.ModifierType;
 import scrabble.model.Tile;
 import scrabble.model.TileScore;
-import scrabble.network.messages.PlayTiles;
 import scrabble.view.frame.TileButton;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class GameScreen extends JPanel {
 
-	private JLabel gameTime;
-	private JButton[][] gameCells;
-	public Tile[][] letters = new Tile[Board.BOARD_ROWS][Board.BOARD_COLUMNS];
-	private RackPanel rackPanel;
+	public static final Color DOUBLE_WORD = new Color(255, 102, 102);
+	public static final Color TRIPLE_WORD = new Color(255, 0, 0);
+	public static final Color DOUBLE_LETTER = new Color(88, 117, 255);
+	public static final Color TRIPLE_LETTER = new Color(0, 41, 255);
+	public static final Color NORMAL_CELL = new Color(255, 255, 255);
+	public static final int GAP = 175;
+	public static final int RACK_SIZE = 7;
 
+	private JLabel gameTime;
+	private BoardPanel boardPanel;
+	public Tile[][] letters = new Tile[Board.BOARD_ROWS][Board.BOARD_COLUMNS];
+
+	private RackPanel rackPanel;
 	private JButton submitButton;
 	private Board board = new Board();
 
-	public List<Tile> getPlayedTiles() {
-		return playedTiles;
-	}
 
 	public List<Tile> playedTiles = new ArrayList<>();
 	private JButton value = new JButton(" ");
-	public static final Color doubleWord = new Color(255, 102, 102);
-	public static final Color tripleWord = new Color(255, 0, 0);
-	public static final Color doubleLetter = new Color(88, 117, 255);
-	public static final Color tripleLetter = new Color(0, 41, 255);
-	public static final Color normalCell = new Color(255, 255, 255);
-	public static final int GAP = 175;
-	public static final int RACKSIZE = 7;
-
-	public void setValue(JButton value) {
-		this.value = value;
-		System.out.println(value.getText());
-	}
-
-	public JButton getValue() {
-		return value;
-	}
 
 	public GameScreen() {
 		this.setLayout(new BorderLayout());
 
-		JPanel northPanel = new JPanel(new FlowLayout());
-		gameTime = new JLabel("00:00");
-		gameTime.setBorder(BorderFactory.createEtchedBorder());
-		northPanel.add(gameTime);
-
-		JPanel centerPanel = new JPanel(new FlowLayout());
-		centerPanel.setBorder(BorderFactory.createTitledBorder("Game Board"));
-		JPanel gamePanel = new JPanel(new GridLayout(15,15,3,3));
-		gameCells = new JButton[15][15];
-		for (int i = 0; i < 15; i++) {
-			for(int j = 0; j < 15; j++) {
-
-				// Get modifier for special cells like Double Word, Triple Word, etc.
-				JButton boardTile = new JButton(" ");
-
-				ModifierType mt = board.getBoardSpecialCell().get(new Point(i, j));
-				if(mt != null) {
-					if (mt == ModifierType.DOUBLE_WORD) {
-						boardTile.setBackground(doubleWord);
-						boardTile.setText("DW");
-						boardTile.setBorderPainted(false);
-					} else if (mt == ModifierType.TRIPLE_WORD) {
-						boardTile.setBackground(tripleWord);
-						boardTile.setText("TW");
-						boardTile.setBorderPainted(false);
-					} else if (mt == ModifierType.DOUBLE_LETTER) {
-						boardTile.setBackground(doubleLetter);
-						boardTile.setText("DL");
-						boardTile.setBorderPainted(false);
-					} else if (mt == ModifierType.TRIPLE_LETTER) {
-						boardTile.setBackground(tripleLetter);
-						boardTile.setText("TL");
-						boardTile.setBorderPainted(false);
-					} else {
-						boardTile.setBackground(normalCell);
-					}
-				}
-
-				gameCells[i][j] = boardTile;
-				gamePanel.add(boardTile);
-			}
-		}
-		centerPanel.add(gamePanel);
+		JPanel northPanel = setupNorthPanel();
+		JPanel centerPanel = setupCenterPanel();
 
 		JPanel eastPanel = new JPanel(new GridLayout(2,1,0,GAP));
 		JPanel westPanel = new JPanel(new GridLayout(2,1,0,GAP));
@@ -110,19 +54,7 @@ public class GameScreen extends JPanel {
 		eastPanel.add(player2Panel);
 		eastPanel.add(player3Panel);
 
-		JPanel southPanel = new JPanel(new FlowLayout());
-		JPanel submitAndRack = new JPanel(new GridLayout(2,1,0,10));
-		submitButton = new JButton("Submit");
-		TilePanel[] tilePanels = new TilePanel[RACKSIZE];
-		for (int i = 0; i < tilePanels.length; i++) {
-			tilePanels[i] = new TilePanel(new TileButton(TileScore.values()[i]));
-		}
-		this.rackPanel = new RackPanel(tilePanels);
-
-
-		submitAndRack.add(submitButton);
-		submitAndRack.add(rackPanel);
-		southPanel.add(submitAndRack);
+		JPanel southPanel = setupSouthPanel();
 
 		//Drop down menu
 		JComboBox<String> comboBox = getStringJComboBox();
@@ -139,27 +71,6 @@ public class GameScreen extends JPanel {
 //		submitActionListener();
 	}
 
-	private static JComboBox<String> getStringJComboBox() {
-		String[] options = {"Rules", "Game Audio", "Game FX", "Quit"};
-		JComboBox<String> comboBox = new JComboBox<>(options);
-		comboBox.setBounds(0, 0, 100, 25);
-
-		comboBox.setAction(new AbstractAction() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(e.getSource() == comboBox) {
-					String menu = options[comboBox.getSelectedIndex()];
-					switch (menu) {
-						case "Rules" -> System.out.println("There are no rules man, We laust!");
-						case "Game Audio" -> System.out.println("No audio for now :( ");
-						case "Game FX" -> System.out.println("No FX either :( ");
-						case "Quit" -> System.out.println("I wish dude!");
-                    }
-				}
-			}
-		});
-		return comboBox;
-	}
 	/*
 	public void boardTilesActionListener(){
 		for (int i = 0; i < Board.BOARD_ROWS; i++) {
@@ -265,12 +176,26 @@ public class GameScreen extends JPanel {
 
 	 */
 
-	public JLabel getGameTime() {
-		return gameTime;
+
+	public List<Tile> getPlayedTiles() {
+		return playedTiles;
 	}
 
-	public JButton[][] getGameCells() {
-		return gameCells;
+	public void setValue(JButton value) {
+		this.value = value;
+		System.out.println(value.getText());
+	}
+
+	public JButton getValue() {
+		return value;
+	}
+
+	public BoardPanel getBoardPanel() {
+		return boardPanel;
+	}
+
+	public JLabel getGameTime() {
+		return gameTime;
 	}
 
 	public RackPanel getRackPanel() {
@@ -279,6 +204,99 @@ public class GameScreen extends JPanel {
 
 	public JButton getSubmitButton() {
 		return submitButton;
+	}
+
+	private JPanel setupSouthPanel() {
+		JPanel southPanel = new JPanel(new FlowLayout());
+		JPanel submitAndRack = new JPanel(new GridLayout(2,1,0,10));
+		submitButton = new JButton("Submit");
+		TilePanel[] tilePanels = new TilePanel[RACK_SIZE];
+		for (int i = 0; i < tilePanels.length; i++) {
+			tilePanels[i] = new TilePanel(new TileButton(TileScore.values()[i]));
+		}
+		this.rackPanel = new RackPanel(tilePanels);
+
+
+		submitAndRack.add(submitButton);
+		submitAndRack.add(rackPanel);
+		southPanel.add(submitAndRack);
+		return southPanel;
+	}
+
+	private JPanel setupCenterPanel() {
+		JPanel centerPanel = new JPanel(new FlowLayout());
+		centerPanel.setBorder(BorderFactory.createTitledBorder("Game Board"));
+
+//		JPanel gamePanel = new JPanel(new GridLayout(15,15,3,3));
+		BoardCellPanel[][] boardCellPanels = new BoardCellPanel[Board.BOARD_ROWS][Board.BOARD_COLUMNS];
+//		gameCells = new JButton[15][15];
+		for (int i = 0; i < boardCellPanels.length; i++) {
+			for(int j = 0; j < boardCellPanels[0].length; j++) {
+
+				// Get modifier for special cells like Double Word, Triple Word, etc.
+				JButton boardTile = new JButton(" ");
+
+				ModifierType mt = board.getBoardSpecialCell().get(new Point(i, j));
+				if(mt != null) {
+					if (mt == ModifierType.DOUBLE_WORD) {
+						boardTile.setBackground(DOUBLE_WORD);
+						boardTile.setText("DW");
+						boardTile.setBorderPainted(false);
+					} else if (mt == ModifierType.TRIPLE_WORD) {
+						boardTile.setBackground(TRIPLE_WORD);
+						boardTile.setText("TW");
+						boardTile.setBorderPainted(false);
+					} else if (mt == ModifierType.DOUBLE_LETTER) {
+						boardTile.setBackground(DOUBLE_LETTER);
+						boardTile.setText("DL");
+						boardTile.setBorderPainted(false);
+					} else if (mt == ModifierType.TRIPLE_LETTER) {
+						boardTile.setBackground(TRIPLE_LETTER);
+						boardTile.setText("TL");
+						boardTile.setBorderPainted(false);
+					} else {
+						boardTile.setBackground(NORMAL_CELL);
+					}
+				}
+				boardCellPanels[i][j] = new BoardCellPanel(boardTile);
+
+//				gameCells[i][j] = boardTile;
+//				gamePanel.add(boardTile);
+			}
+		}
+		boardPanel = new BoardPanel(boardCellPanels);
+		centerPanel.add(boardPanel);
+		return centerPanel;
+	}
+
+	private JPanel setupNorthPanel() {
+		JPanel northPanel = new JPanel(new FlowLayout());
+		gameTime = new JLabel("00:00");
+		gameTime.setBorder(BorderFactory.createEtchedBorder());
+		northPanel.add(gameTime);
+		return northPanel;
+	}
+
+	private static JComboBox<String> getStringJComboBox() {
+		String[] options = {"Rules", "Game Audio", "Game FX", "Quit"};
+		JComboBox<String> comboBox = new JComboBox<>(options);
+		comboBox.setBounds(0, 0, 100, 25);
+
+		comboBox.setAction(new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource() == comboBox) {
+					String menu = options[comboBox.getSelectedIndex()];
+					switch (menu) {
+						case "Rules" -> System.out.println("There are no rules man, We laust!");
+						case "Game Audio" -> System.out.println("No audio for now :( ");
+						case "Game FX" -> System.out.println("No FX either :( ");
+						case "Quit" -> System.out.println("I wish dude!");
+					}
+				}
+			}
+		});
+		return comboBox;
 	}
 
 	private JPanel setupPlayerPanel() {
