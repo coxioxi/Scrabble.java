b590d21d5ba8b06c1aaac9caf835472b00461738
Moose Lat <mooselat12@gmail.com>
2024-11-06 16:36:48 -0500

    Added action listeners for main menu buttons

--- code/scrabble/controller/MainMenuController.java
+++ code/scrabble/controller/MainMenuController.java
@@ -13,6 +13,19 @@ public class MainMenuController {
 	}
 
 	private void addActionListeners() {
-
+		menuScreen.getHostButton().addActionListener(e -> parent.getView().showHost());
+		menuScreen.getJoinButton().addActionListener(e -> parent.getView().showJoin());
+		menuScreen.getAudioCheck().addActionListener(e -> audioCheckChanged());
+		menuScreen.getFxCheck().addActionListener(e -> fxCheckChanged());
+		menuScreen.getQuitButton().addActionListener(e -> parent.getView().dispose());
 	}
+
+	private void fxCheckChanged() {
+		//fx is not yet implemented
+	}
+
+	private void audioCheckChanged() {
+		//Audio is not yet implemented
+	}
+
 }
