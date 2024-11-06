a1e3dd7a55bc7c3228dea59d04791c4a886c2265
samuel <costa2@otterbein.edu>
2024-11-05 22:33:49 -0500

    Created more tests in Test.java and added some getters for testing purposes in Board.java

--- code/scrabble/model/Board.java
+++ code/scrabble/model/Board.java
@@ -892,7 +892,14 @@ public class Board {
 				arePointsConnected(tiles));
     }
 
-	/*
+    //getters for testing purposes
+    public boolean getAreInBounds(Tile[] tiles){return arePointsInbounds(tiles);}
+    public boolean getHasNoDuplicates(Tile[] tiles){return hasNoDuplicates(tiles);}
+    public boolean getPointsNotOccupied(Tile[] tiles){return pointsNotOccupied(tiles);}
+    public boolean getArePointsStartingOrAdjacent(Tile[] tiles){return arePointsStartingOrAdjacent(tiles);}
+
+
+    /*
 	helper method which checks that all tiles are adjacent
 	to each other or have gaps filled with board tiles
 	 */
