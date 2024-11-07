9a621d32456de11d1fcc59d250c671c7c5b3d7d5
Moose Lat <mooselat12@gmail.com>
2024-11-06 23:24:30 -0500

    added implementation for the insertion of hosts port in the text field

--- code/scrabble/controller/JoinScreenController.java
+++ code/scrabble/controller/JoinScreenController.java
@@ -9,6 +9,7 @@ public class JoinScreenController {
     private final JoinScreen joinScreen;
     private String userName;
     private String hostsIP;
+    private String hostsPort;
 
     public JoinScreenController (Controller parent, JoinScreen joinScreen) {
         this.parent = parent;
@@ -20,19 +21,24 @@ public class JoinScreenController {
     private void joinClick() {
         userName = joinScreen.getNameTextField().getText();
         hostsIP = joinScreen.getIPTextField().getText().trim();
+        hostsPort = joinScreen.getPort().getText().trim();
         if (userName.isBlank()) {
             parent.getView().showNoNameDialog();
         } else if (hostsIP.isBlank()) {
             parent.getView().showNoIPDialog();
+        } else if (hostsPort.isBlank()) {
+            parent.getView().showNoPortDialog();
         } else {
+            int portNumber;
             try {
-                parent.setupSocket(hostsIP);
+                portNumber = Integer.parseInt(hostsPort);
+                parent.setupSocket(hostsIP, portNumber);
                 parent.getView().showWaiting();
+            } catch (NumberFormatException e) {
+                parent.getView().showNoPortDialog();
             } catch (IOException e) {
-                System.out.println(e);
                 parent.getView().showIPErrorDialog();
             }
-
         }
     }
 
