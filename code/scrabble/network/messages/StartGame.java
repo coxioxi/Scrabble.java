af90057c020f076e4a4c037c7cd90929ca377d53
samuel <costa2@otterbein.edu>
2024-11-07 21:30:18 -0500

    Implemented shuffling turnID inside the execute method

--- code/scrabble/network/messages/StartGame.java
+++ code/scrabble/network/messages/StartGame.java
@@ -1,15 +1,22 @@
 package scrabble.network.messages;
 
 import scrabble.controller.Controller;
+import scrabble.model.Player;
 import scrabble.model.Ruleset;
 import scrabble.model.Tile;
+import scrabble.network.host.HostReceiver;
 import scrabble.network.host.PartyHost;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 public class StartGame extends Message {
 	private int[] playerIDs;
 	private int receivingID;
 	private Ruleset ruleset;
 	private Tile[] startingTiles;
+	HashMap<Player, Integer> turnID = new HashMap<>();
 
 	public StartGame(int senderID, int receivingID, int[] playerIDs, Ruleset ruleset, Tile[] startingTiles) {
 		super(senderID);
@@ -50,6 +57,34 @@ public class StartGame extends Message {
 		// or make it possible to instantiate new game object for players when executing message
 		try {
 			controller.getMessenger().sendMessage(this);
+
+			int[] randomNumbers = new int[controller.getModel().getPlayers().length];
+			for (int i = 0; i < randomNumbers.length; i++) {
+				randomNumbers[i] = i;
+			}
+
+			//shuffle randomNumbers array so the player order is randomised
+			Random random = new Random();
+			for (int i = 0; i < randomNumbers.length;) {
+				int index = random.nextInt(randomNumbers.length);
+				int temp;
+				if (index != i) {
+					temp = randomNumbers[index];
+					randomNumbers[index] = randomNumbers[i];
+					randomNumbers[i] = temp;
+					++i;
+				}
+			}
+
+			int i = 0;
+			for(Player player: turnID.keySet()){
+				turnID.replace(player, randomNumbers[i]);
+				++i;
+			}
+
+			for (int j = 0; j < controller.getModel().getPlayers().length; ++i)
+				controller.getModel().getPlayers()[i].setTurnID(turnID.get(controller.getModel().getPlayers()[i]));
+
 			controller.getModel().setRuleset(ruleset);
 			controller.getView().setupGameScreen(ruleset, controller.getModel().getPlayers(), controller.getModel().getPlayers().length);
 		} catch (Exception e) {
