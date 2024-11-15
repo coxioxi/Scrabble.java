a0ef4c5f92b56e95b4b85157e17bf8abd1b0e5d4
samuel <samukaka1000@gmail.com>
2024-11-15 12:34:36 -0800

    Documentation

--- code/scrabble/network/messages/AssignID.java
+++ code/scrabble/network/messages/AssignID.java
@@ -1,10 +1,16 @@
 package scrabble.network.messages;
+/*
+ * Authors: Ian Boyer, David Carr, Samuel Costa, Maximus Latkovski, Jy'el Mason
+ * Course: COMP 3100
+ * Instructor: Dr. Barry Wittman
+ * Original date: 10/08/2024
+ */
 
 import scrabble.controller.Controller;
 import scrabble.network.host.PartyHost;
 
 /**
- * This class is responsible for assigning IDs to players.
+ * This class is responsible for assigning IDs to players
  */
 public class AssignID extends Message{
 	private int playerID;
